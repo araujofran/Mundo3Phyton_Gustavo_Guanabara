aa07e6690d7012fbeb776b84bc22dbcf5530ea8d
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-28 06:51:39 -0300

    D01

--- desafio01.py
+++ desafio01.py
@@ -1,31 +1,19 @@
 
-digite = 0
 count=0
-digite = int (input('Digite um número de 0 a 5 : '))
-while digite <=5:
-    if count > 10:
-        break
 extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco')
 
-print (15*'-=')
 
-print (f'O numeral  digitado foi {digite} e a expressão por extenso é {extenso[digite]}!')
+for count in extenso:
 
-print('\n')
-count +=1
-while digite >5:
-    denovo= input ('Tente Novamente!!! Digite um numero entre 0 e 5 :')
-count=0
-while denovo <=5 :
-    extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco')
+   digite = int (input('Digite um número de 0 a 5 : '))
+   print (f'O numeral  digitado foi {digite} e a expressão por extenso é {extenso[digite]}!')
+if digite>5 or digite<0:
+    
+    digite = int (input('Digite novamente um número de 0 a 5 : '))
+    
+    print (f'O numeral  digitado foi {digite} e a expressão por extenso é {extenso[digite]}!')
 
-    print (15*'-=')
+else:
+    print (f'O numeral  digitado foi {digite} e a expressão por extenso é {extenso[digite]}!')
 
-    print (f'O numeral  digitado foi {denovo} e a expressão por extenso é {extenso[denovo]}!')
-       
-    print('\n')
-    count +=1
-    if count > 10:
-        break
 
-    print('Obrigado')
