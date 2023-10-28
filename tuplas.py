bae71af47649c9738834b569f13393a74cc687e4
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-27 21:08:06 -0300

    aprendendo tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,5 +1,13 @@
 legumes = ('A','B','C','D','E')
 
+print (len(legumes))
+
 for pos, comida in enumerate(legumes):
     print (f'Eu vou selecionar {comida} na posição {pos}')
 
+print ('\n')
+print(f'A letra C está em qual posição? Esta na posição: {legumes.index('C')}')
+print ('\n')
+print(f'A letra C aparece : {legumes.count('C')} veze(s)')
+
+print ('\n')
