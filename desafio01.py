669c5015ed6fe3a06e65e2b0f533e57c74ac8138
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-28 09:53:06 -0300

    Consegui! Ex01 Tuplas
    Ufá!

--- desafio01.py
+++ desafio01.py
@@ -3,15 +3,29 @@
 extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco')
 
 while True:
-   
-   digite = int (input('Digite um número de 0 a 5 : '))
-   
-   if 0<= digite <=5:
-        break
-   
-   print('Digite novamente.', end=' ')
-    
-print (f'O numero digitado foi {digite} e a frase por extenso é {extenso[digite]}!')  
+     
+     digite = int (input('Digite um número de 0 a 5 : '))
+     if 0<= digite <=5:
+          break
+         
+     print('Digite novamente.', end=' ')
+print (f'O numero digitado foi {digite} e a frase por extenso é {extenso[digite]}!')
+resp = str (input('Você quer continuar? [S/N] ')).strip().upper() 
 
+while resp  in 'S':
+     while True:
+     
+          digite = int (input('Digite um número de 0 a 5 : '))
+          if 0<= digite <=5:
+               break
+         
+          print('Digite novamente.', end=' ')
+     print (f'O numero digitado foi {digite} e a frase por extenso é {extenso[digite]}!')
+     resp = str (input('Você quer continuar? [S/N] ')).strip().upper()
+     if resp == 'N':
+          break
+
+print (f'O numero digitado foi {digite} e a frase por extenso é {extenso[digite]}!')
+print ('OBRIGADO!')
 
 
